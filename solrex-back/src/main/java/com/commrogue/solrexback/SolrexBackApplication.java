37486ac28c1a2a60fd851f176880ead327a1af2b
CommRogue <55894360+CommRogue@users.noreply.github.com>
2024-06-11 15:02:42 +0300

    Initial Commit

---
+++ solrex-back/src/main/java/com/commrogue/solrexback/SolrexBackApplication.java
@@ -0,0 +1,13 @@
+package com.commrogue.solrexback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrexBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrexBackApplication.class, args);
+	}
+
+}
---
+++ solrex-back/src/test/java/com/commrogue/solrexback/SolrexBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commrogue.solrexback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrexBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
