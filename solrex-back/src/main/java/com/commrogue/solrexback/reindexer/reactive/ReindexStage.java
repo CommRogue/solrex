eb125dd15acf74be88777b2b5219a71b38e7ab4d
CommRogue <55894360+CommRogue@users.noreply.github.com>
2024-06-28 23:34:56 +0300

    backup

--- solrex-back/src/main/java/com/commrogue/solrexback/reindexer/reactive/ReindexStage.java
+++ solrex-back/src/main/java/com/commrogue/solrexback/reindexer/reactive/ReindexStage.java
@@ -1,10 +1,12 @@
 package com.commrogue.solrexback.reindexer.reactive;
 
 import lombok.Data;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.springframework.beans.factory.annotation.Value;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Data
 public class ReindexStage {
@@ -14,8 +16,19 @@ public class ReindexStage {
 
     private final ShardMapping shardMapping;
 
+    @Value("${reindexer.dataimport-rh}")
+    String diRequestHandler;
 
-    public Mono<Integer> run() {
-        return Mono.empty();
+    public Mono<Void> run() {
+        return this.getShardMapping().forEach((dstClient, sources) -> {
+            SolrQuery dataImportRequest = new SolrQuery();
+            dataImportRequest.set("qt", diRequestHandler);
+            dataImportRequest.set("command", "full-import");
+            dataImportRequest.set("commit", "true");
+            SolrQuery statusRequest = new SolrQuery();
+            statusRequest.set("qt", diRequestHandler);
+            statusRequest.set("command", "status");
+            Mono.fromCompletionStage(dstClient.query(dataImportRequest)).then(Mono.fromCompletionStage(dstClient.query(statusRequest))).()
+        });
     }
 }
